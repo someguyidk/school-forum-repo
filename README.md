a887a6bf8b757a3dec49ee4bcb5b9df16bd128ed
someguyidk <7carloselias@gmail.com>
2022-02-11 12:32:27 -0600

    Added msg in Venn branch README.md

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ add
 commit
 pull
 push
+
+and also learn the branch commands,
+pls work on the branch called "Venn",
+when pushing, pls type "git push origin Venn"
--- forumapp/forum/models.py
+++ forumapp/forum/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Forum post db model
+class For_Post(models.Model):
+	title = models.CharField(max_length=150)
+	#content
+	#date_posted
+
+
+#So the db model for possibly maybe polls?
+
+#class For_Polls(models.Model):
+#	options = list?
+#	amount of oprtions = len(options)
+
+
+
