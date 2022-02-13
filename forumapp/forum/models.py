2da0218997866646409ac87ea34dcf1b638bbe0d
someguyidk <7carloselias@gmail.com>
2022-02-13 08:07:14 -0600

    Committing change to the models.py file to merge from main to someguyidk branch

--- forumapp/forum/models.py
+++ forumapp/forum/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Forum post db model
+class For_Post(models.Model):
+	id = models.AutoField(primary_key = True)
+	title = models.CharField(max_length=150)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	#When user (author) is deleted so is their posts
+	author = models.ForeignKey(User, on_delete = models.CASCADE)
+	#AN = "annonymous"
+	AN = models.BooleanField(default=False)
+	file = models.FileField()
+
+	def __str__(self):
+		return self.title
+#So the db model for possibly maybe polls?
+
+#class For_Polls(models.Model):
+class For_Polls(models.Model):
+	id = models.AutoField(primary_key = True)
+	title = models.CharField(max_length=150)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	#When user (author) is deleted so is their posts
+	author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+
+	def __str__(self):
+		return self.title
